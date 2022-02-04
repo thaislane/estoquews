a8a5d898ce39290a3b20f62eca5ccd4dd19547c9
Thaislane <thaislanesteves@hotmail.com>
2022-02-03 21:14:02 -0300

    Personalizando o serviço

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -2,20 +2,25 @@ package br.com.caelum.estoque.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
 	
 	private ItemDao dao = new ItemDao();
 	
-	public List<Item> getitens(){
+	@WebMethod(operationName = "todosOsItens")
+	@WebResult(name = "itens")
+	public ListaItens getitens(){
 		System.out.println("Chamando getItens()");
 		List<Item> lista = dao.todosItens();
-		return lista;
+		return new ListaItens(lista);
 	}
 
 }
