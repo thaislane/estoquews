faf5c74cc421c14a72b19acfb63977d742d74e1f
Thaislane <thaislanesteves@hotmail.com>
2022-02-04 10:01:52 -0300

    Trabalhando com cabeçalhos, Testando o Header, Verificando o token e Trabalhando com exceções

---
+++ src/br/com/caelum/estoque/modelo/usuario/AutorizacaoException.java
@@ -0,0 +1,16 @@
+package br.com.caelum.estoque.modelo.usuario;
+
+import javax.xml.ws.WebFault;
+
+@WebFault(name = "AutorizacaoFault")
+public class AutorizacaoException extends Exception {
+
+	public AutorizacaoException(String mensagem) {
+		super(mensagem);
+	}
+	
+	public String getFaultInfo() {
+		return "Token invalido";
+	}
+
+}
--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -13,6 +13,10 @@ import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.AutorizacaoException;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
+import br.com.caelum.estoque.modelo.usuario.Usuario;
 
 @WebService
 public class EstoqueWS {
@@ -49,11 +53,29 @@ public class EstoqueWS {
 //	@RequestWrapper(localName="listaItens")
 //	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
 //
-//	List<Filtro> lista = filtros.getLista();
-//	List<Item> result = dao.todosItens(lista);
+//		List<Filtro> lista = filtros.getLista();
+//		List<Item> result = dao.todosItens(lista);
 //
-//	return new ListaItens(result);
-//
+//		return new ListaItens(result);
 //	}
+	
+	@WebMethod(operationName = "CadastrarItem")
+	@WebResult(name = "item")
+	public Item cadastrarItem(
+			@WebParam(name = "tokenUsuario", header = true) TokenUsuario token, 
+			@WebParam(name = "item")Item item) 
+					throws AutorizacaoException {
+		
+		System.out.println("cadastrando item " + item + ", " + token);
+		boolean valido = new TokenDao().ehValido(token);
+		
+		if(!valido) {
+			throw new AutorizacaoException("Autorizacao falhou");
+		}
+		
+		this.dao.cadastrar(item);
+		return item;
+		
+	}
 
 }
