cc8d0eff5c53d207391774af6ee0f5bbc172dcbb
Thaislane <thaislanesteves@hotmail.com>
2022-02-03 21:57:31 -0300

    Filtrando Resultados

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -3,9 +3,13 @@ package br.com.caelum.estoque.ws;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.ResponseWrapper;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
@@ -17,10 +21,39 @@ public class EstoqueWS {
 	
 	@WebMethod(operationName = "todosOsItens")
 	@WebResult(name = "itens")
-	public ListaItens getitens(){
+	public ListaItens getitens(@WebParam(name = "filtros")Filtros filtros){
+		
 		System.out.println("Chamando getItens()");
-		List<Item> lista = dao.todosItens();
-		return new ListaItens(lista);
+		
+		List<Filtro> lista = filtros.getLista();
+		List<Item> itensResultado = dao.todosItens(lista);
+		return new ListaItens(itensResultado);
 	}
+	
+	
+	/**ResponseWrapper */
+//	@WebMethod(operationName="todosOsItens")
+//	@ResponseWrapper(localName="itens")
+//	@WebResult(name="item")
+//	public List<Item> getItens() { 
+//
+//	    System.out.println("Chamando getItens()");
+//	    return dao.todosItens();
+//
+//	}
+	
+	/**RequestWrapper*/
+//	@WebMethod(operationName="todosOsItens")
+//	@ResponseWrapper(localName="itens")
+//	@WebResult(name="item")
+//	@RequestWrapper(localName="listaItens")
+//	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros){
+//
+//	List<Filtro> lista = filtros.getLista();
+//	List<Item> result = dao.todosItens(lista);
+//
+//	return new ListaItens(result);
+//
+//	}
 
 }
